186c2af626de7640e62a4864c36ced8f32fa7b51
Andi Asrafil <andi.asrafil@kompas.com>
2022-05-07 01:18:07 +0700

    Initial Commit

---
+++ CryptoWatcher/CryptoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWatcherApp.swift
+//  CryptoWatcher
+//
+//  Created by TI Digital on 07/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
